f7cd766a0b9abe10bbeaf42d73107f045151d45a
Bandeira <bandeiradev.ti@gmail.com>
2025-07-06 12:06:19 -0300

    :sparkles: feat: Hover nos icones

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,21 @@ function Footer() {
       <div className="bg-black p-8 flex justify-between">
         <h3 className="text-white font-bold">Z-Geek©2025</h3>
         <div className="flex flex-row gap-2">
-          <Instagram color="white" size={23} />
-          <Github color="white" size={23} />
-          <Linkedin color="white" size={23} />
+          <Instagram
+            color="white"
+            size={23}
+            className="hover:scale-105 transition cursor-pointer"
+          />
+          <Github
+            color="white"
+            size={23}
+            className="hover:scale-105 transition cursor-pointer"
+          />
+          <Linkedin
+            color="white"
+            size={23}
+            className="hover:scale-105 transition cursor-pointer"
+          />
         </div>
       </div>
     </>
