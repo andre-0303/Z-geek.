501b1f9cf04499a90059e81b3720cff3eac5c223
Bandeira <bandeiradev.ti@gmail.com>
2025-07-05 21:16:16 -0300

    :sparkles: feat: Adicionando botao de logout na home

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,38 @@
-const HomePage = () => (
-  <>
-    <h1>ababa</h1>
-  </>
-);
+import { useAuth } from "../App";
+import { useNavigate } from "react-router-dom";
+import { supabase } from "../lib/supabaseClient";
+
+const HomePage = () => {
+  const navigate = useNavigate();
+  const { logout } = useAuth();
+
+  async function handleLogout() {
+    const { error } = await supabase.auth.signOut();
+
+    if (error) {
+      alert(`Erro ao sair: ${error.message}`);
+      return;
+    }
+
+    logout();
+    navigate("/login");
+  }
+
+  return (
+    <>
+      <div>
+        <div className="flex flex-row justify-between p-5">
+          <h1>ababa</h1>
+          <button
+            onClick={handleLogout}
+            className="bg-gamer-primary p-2 px-10 rounded-md"
+          >
+            Sair
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default HomePage;
