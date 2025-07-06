aefad5051bfbde959c9d135915f61be7daa66ceb
Bandeira <bandeiradev.ti@gmail.com>
2025-07-06 12:18:50 -0300

    :iphone:feat: Melhorando a resposividade

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,6 +1,7 @@
 import { useAuth } from "../App";
 import { useNavigate } from "react-router-dom";
 import { supabase } from "../lib/supabaseClient";
+import { LogOut } from "lucide-react";
 
 const HomePage = () => {
   const navigate = useNavigate();
@@ -27,18 +28,21 @@ const HomePage = () => {
       <div className="relative bg-[url('/banner.jpg')] bg-cover bg-center min-h-screen">
         <div className="absolute inset-0 bg-black bg-opacity-50"></div>
 
-        <div className="relative z-10 flex p-5 justify-end">
+        <div className="relative z-10 flex p-4 justify-end">
           <button
             onClick={handleLogout}
-            className="bg-gamer-primary p-2 px-10 rounded-md text-white font-bold"
+            className="bg-gamer-primary p-2 px-5 rounded-md text-white font-bold items-center gap-2"
           >
-            Sair
+            <LogOut className="w-6 h-6 md:hidden" />
+            <span className="hidden md:inline">Sair</span>
           </button>
         </div>
 
-        <div className="relative z-10 flex justify-center items-center min-h-[80vh] flex-col">
-          <h1 className="text-white text-4xl font-bold">Bem-vindo ao Z-Geek</h1>
-          <h3 className="text-gray-300 text-xl mt-4">
+        <div className="relative z-10 flex text-center justify-center items-center min-h-[80vh] flex-col">
+          <h1 className="text-white text-2xl font-bold md:text-4xl">
+            Bem-vindo ao Z-Geek
+          </h1>
+          <h3 className="text-gray-300 text-lg mt-4 md:text-xl mt-2">
             Aqui você encontra as melhores indicações de games da atualidade!
           </h3>
           <button
