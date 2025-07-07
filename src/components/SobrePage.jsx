30ed3c427d9b45cb97f7ba405030b09045906f58
Bandeira <bandeiradev.ti@gmail.com>
2025-07-07 13:13:47 -0300

    :sparkles: feat: Adicionando página de sobre

--- src/components/SobrePage.jsx
+++ src/components/SobrePage.jsx
@@ -1,7 +1,30 @@
 const SobrePage = () => (
-  <>
-    <h1>ababa</h1>
-  </>
+  <div className="min-h-[80vh] flex flex-col justify-center items-center text-gamer-textSecondary bg-gamer-textSecondary px-6 py-12">
+    <h1 className="text-4xl font-extrabold text-gamer-primary mb-8 text-center drop-shadow">
+      Sobre Nós
+    </h1>
+
+    <div className="max-w-3xl text-center text-lg leading-relaxed bg-gamer-gray p-6 rounded-xl shadow-lg">
+      <p className="mb-6">
+        <span className="font-bold text-gamer-primary">Z-Geek</span> nasceu da
+        paixão por games, tecnologia e cultura geek. Nosso objetivo é conectar
+        jogadores e pessoas comuns atráves dos games que estão presentes no
+        nosso mundo.
+      </p>
+
+      <p className="mb-6">
+        Acreditamos que o mundo geek vai muito além dos jogos: é sobre
+        criatividade, inovação e compartilhar experiências inesquecíveis. É por
+        isso que estamos sempre em busca das últimas tendências, novidades e
+        lançamentos para manter você no centro desse universo.
+      </p>
+
+      <p>
+        Seja bem-vindo ao nosso mundo! Fique à vontade para explorar, interagir
+        e fazer parte desse time que não para de crescer.
+      </p>
+    </div>
+  </div>
 );
 
 export default SobrePage;
